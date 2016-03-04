155e77625c877bc259b7e6233ecc6a646f8bc583
Hans Burbano <hburbano@bbox.co>
2016-03-04 07:38:30 -0500

    first commit

---
+++ Assets/Code/Planet.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Planet : MonoBehaviour
+{
+
+    //public GameObject planets;
+    float orbit;
+    float scale;
+    float maxSize = 1.2f;
+    float minSize = 0.2f;
+    int frequency = 360;
+
+    // Use this for initialization
+    void Start()
+    {
+        orbit = this.transform.position.z;
+        scale = Random.Range(minSize, maxSize);
+        this.transform.localScale = new Vector3(scale,scale,scale);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float pos = (maxSize - scale) * (Time.realtimeSinceStartup % frequency);
+        this.transform.position = new Vector3(orbit*Mathf.Sin(pos), 0, orbit * Mathf.Cos(pos));
+        //this.transform.Rotate(Vector3.forward * 0.1f);
+    }
+}
---
+++ Assets/Code/Universo.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Universo : MonoBehaviour {
+
+    public GameObject planets;
+    
+	// Use this for initialization
+	void Start () {
+
+        //
+        Vector3 pos;
+        for(int i = 0; i< 9; i++)
+        {
+            pos = new Vector3(0,0, ((i+1)*20)+Random.Range(0, 2));
+            Instantiate(planets, pos, Quaternion.identity);            
+        }        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	   
+	}
+}
